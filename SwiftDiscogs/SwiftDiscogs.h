cc37b939cb6afb8ff4cd3d006bef7867fa84304c
Jason R Tibbetts <jason@tibbetts.net>
2018-02-21 01:00:29 -0500

    Initial Commit

---
+++ SwiftDiscogs/SwiftDiscogs.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDiscogs.h
+//  SwiftDiscogs
+//
+//  Created by Jason R Tibbetts on 2/21/18.
+//  Copyright © 2018 Poikile Creations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftDiscogs.
+FOUNDATION_EXPORT double SwiftDiscogsVersionNumber;
+
+//! Project version string for SwiftDiscogs.
+FOUNDATION_EXPORT const unsigned char SwiftDiscogsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDiscogs/PublicHeader.h>
+
+
