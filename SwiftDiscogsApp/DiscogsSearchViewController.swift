9467082a304ce9ecc69985d51e37230538f51175
Jason R Tibbetts <jason@martiancraft.com>
2018-04-08 00:16:53 -0400

    No ticket: Added unit tests for the DiscogsSearchViewController.

--- SwiftDiscogsApp/DiscogsSearchViewController.swift
+++ SwiftDiscogsApp/DiscogsSearchViewController.swift
@@ -8,15 +8,19 @@ import UIKit
 /// labels.
 final class DiscogsSearchViewController: UIViewController, UISearchControllerDelegate {
 
-    // MARK: Properties
+    // MARK: Outlets
 
+    /// The object responsible for managing the on-screen contents of the
+    /// Discogs search, which helps keep the size of this view controller down.
     @IBOutlet weak var display: DiscogsSearchDisplay?
 
-    /// The search bar that's managed and installed by the `searchController`.
-    fileprivate var searchBar: UISearchBar {
-        return searchController.searchBar
-    }
+    // MARK: Properties
 
+    /// The Discogs client.
+    var discogsClient: Discogs?
+
+    /// The `UISearchController`. It displays its results in a separate view
+    /// controller (the `DiscogsSearchResultsController`).
     fileprivate lazy var searchController: UISearchController = {
         // Load the search results controller from the storyboard.
         let bundle = Bundle(for: type(of: self))
@@ -30,7 +34,7 @@ final class DiscogsSearchViewController: UIViewController, UISearchControllerDel
         return searchController
     }()
 
-    // MARK: - UIViewController
+    // MARK: UIViewController
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,13 +42,32 @@ final class DiscogsSearchViewController: UIViewController, UISearchControllerDel
         display?.setUp(searchController: searchController, navigationItem: navigationItem)
     }
 
-    // MARK: - Actions
+    // MARK: Actions
 
-    @IBAction func signInToDiscogs() {
-        let promise = DiscogsClient.singleton?.authorize(presentingViewController: self,
-                                                         callbackUrlString: AppDelegate.callbackUrl.absoluteString)
-        promise?.then { [weak self] (credential) in
+    @IBAction func signInTapped(source: UIButton?) {
+        signInToDiscogs()
+    }
+
+    @IBAction func signOutTapped(source: UIButton?) {
+        signOutOfDiscogs()
+    }
+
+    // MARK: Other Functions
+
+    /// Sign into the Discogs service, notifying the display when it's about to
+    /// do so and after the user has logged in successfully.
+    ///
+    /// - parameter completion: An optional function that's called after the
+    ///             the user has successfully signed in.
+    func signInToDiscogs(completion: (() -> Void)? = nil) {
+        display?.willSignIn()
+        
+        let promise = discogsClient?.authorize(presentingViewController: self,
+                                               callbackUrlString: AppDelegate.callbackUrl.absoluteString)
+        promise?.then { [weak self] (credential) -> Void in
+            // TODO: Use the user's real name.
             self?.display?.signedInAs(userName: "Fatty Arbuckle")
+            completion?()
             }.catch { (error) in
                 let alertTitle = NSLocalizedString("discogsSignInFailed",
                                                    tableName: nil,
@@ -58,10 +81,17 @@ final class DiscogsSearchViewController: UIViewController, UISearchControllerDel
         }
     }
 
-    @IBAction func signOutOfDiscogs() {
+    /// Sign out of the Discogs service, notifying the display when it's about
+    /// to do so and after the user has logged out successfully.
+    ///
+    /// - parameter completion: An optional function that's called after the
+    ///             the user has successfully signed out.
+    func signOutOfDiscogs(completion: (() -> Void)? = nil) {
         display?.willSignOut()
 
         // TODO: Call the sign-out method that doesn't yet exist.
         display?.signedOut()
+        completion?()
     }
+
 }
---
+++ SwiftDiscogsAppTests/DiscogsSearchViewControllerTests.swift
@@ -0,0 +1,75 @@
+//  Copyright © 2018 Poikile Creations. All rights reserved.
+
+@testable import SwiftDiscogs
+@testable import SwiftDiscogsApp
+import XCTest
+
+class MockSearchDisplay: DiscogsSearchDisplay {
+
+    var setUpCalled = false
+    var signedInAsCalled = false
+    var signedOutCalled = false
+    var willSignInCalled = false
+    var willSignOutCalled = false
+
+    func setUp(searchController: UISearchController, navigationItem: UINavigationItem) {
+        setUpCalled = true
+    }
+
+    func signedInAs(userName: String) {
+        signedInAsCalled = true
+    }
+
+    func signedOut() {
+        signedOutCalled = true
+    }
+
+    func willSignIn() {
+        willSignInCalled = true
+    }
+
+    func willSignOut() {
+        willSignOutCalled = true
+    }
+
+}
+
+class DiscogsSearchViewControllerTests: XCTestCase {
+
+    var searchViewController: DiscogsSearchViewController!
+    var mockSearchDisplay: MockSearchDisplay!
+
+    override func setUp() {
+        super.setUp()
+
+        searchViewController = DiscogsSearchViewController()
+        mockSearchDisplay = MockSearchDisplay()
+        searchViewController.display = mockSearchDisplay
+        searchViewController.discogsClient = MockDiscogs()
+    }
+
+    func testSignInAndOutCallExpectedMethods() {
+        let signInExpectation = expectation(description: "Signing in to Discogs")
+
+        searchViewController.signInToDiscogs() {
+            XCTAssertTrue(self.mockSearchDisplay.willSignInCalled)
+            XCTAssertTrue(self.mockSearchDisplay.signedInAsCalled)
+            XCTAssertFalse(self.mockSearchDisplay.willSignOutCalled)
+            XCTAssertFalse(self.mockSearchDisplay.signedOutCalled)
+            signInExpectation.fulfill()
+        }
+
+        wait(for: [signInExpectation], timeout: 2.0)
+
+        let signOutExpectation = expectation(description: "Signing out of Discogs")
+
+        searchViewController.signOutOfDiscogs() {
+            XCTAssertTrue(self.mockSearchDisplay.willSignOutCalled)
+            XCTAssertTrue(self.mockSearchDisplay.signedOutCalled)
+            signOutExpectation.fulfill()
+        }
+
+        wait(for: [signOutExpectation], timeout: 2.0)
+    }
+
+}
