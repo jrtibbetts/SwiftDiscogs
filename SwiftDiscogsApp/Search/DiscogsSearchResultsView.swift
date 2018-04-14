9c4f836175b1e7530c773ffb5ef2a8161b71b1a2
Jason R Tibbetts <jason@tibbetts.net>
2018-04-14 18:55:52 -0400

    No ticket: Moved the ArtistSearchCell out into its own file.

--- SwiftDiscogsApp/Search/DiscogsSearchResultsView.swift
+++ SwiftDiscogsApp/Search/DiscogsSearchResultsView.swift
@@ -7,31 +7,3 @@ open class DiscogsSearchResultsView: CollectionAndTableDisplay<[DiscogsSearchRes
 
 
 }
-
-open class ArtistSearchResultCell: UITableViewCell {
-
-    open var artistName: String? {
-        didSet {
-            nameLabel?.text = artistName
-        }
-    }
-
-    open var details: String? {
-        didSet {
-            detailsLabel?.text = details
-        }
-    }
-
-    open var thumbnailUrlString: String? {
-        didSet {
-            //            if let thumbnailUrlString = thumbnailUrlString {
-            //                thumbnailView?.image = nil
-            //            }
-        }
-    }
-
-    @IBOutlet fileprivate weak var detailsLabel: UILabel?
-    @IBOutlet fileprivate weak var nameLabel: UILabel?
-    @IBOutlet fileprivate weak var thumbnailView: UIImageView?
-
-}
