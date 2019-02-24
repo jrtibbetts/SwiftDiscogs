0f4f517668eb24e86f88cc418c06fd24d02f1a3a
Jason R Tibbetts <jason@tibbetts.net>
2019-02-23 23:14:09 -0500

    No ticket: Reverted DiscogsCollegeRelease by moving the associated structs back as inner classes.

--- Source/Collection/DiscogsCollectionRelease.swift
+++ Source/Collection/DiscogsCollectionRelease.swift
@@ -4,59 +4,59 @@ import Foundation
 
 public struct DiscogsCollectionRelease: Codable {
     
-    public var basicInformation: [DiscogsCollectionReleaseInformation]?
+    public var basicInformation: [Information]?
     public var folderId: Int
     public var id: Int
     public var instanceId: Int
-    public var notes: [DiscogsCollectionReleaseNote]?
+    public var notes: [Note]?
     public var rating: Int
-    
-}
 
-public struct DiscogsCollectionReleaseArtist: Codable {
-    public var anv: String?
-    public var id: Int
-    public var join: String?
-    public var name: String
-    public var resourceUrl: String
-    public var tracks: String?
-    public var role: String?
-    
-}
+    public struct Artist: Codable {
+        public var anv: String?
+        public var id: Int
+        public var join: String?
+        public var name: String
+        public var resourceUrl: String
+        public var tracks: String?
+        public var role: String?
 
-public struct DiscogsCollectionReleaseInformation: Codable {
-    public var id: Int
-    public var title: String
-    public var year: Int?
-    public var resourceUrl: String
-    public var thumb: String?
-    public var coverImage: String?
-    public var formats: [DiscogsCollectionReleaseFormat]?
-    public var labels: [DiscogsCollectionReleaseLabel]?
-    public var artists: [DiscogsCollectionReleaseArtist]?
-    
-}
+    }
 
-public struct DiscogsCollectionReleaseFormat: Codable {
-    public var descriptions: [String]?
-    public var name: String?
-    public var quantity: String? { return qty }
-    public var qty: String?
-    
-}
+    public struct Format: Codable {
+        public var descriptions: [String]?
+        public var name: String?
+        public var quantity: String? { return qty }
+        public var qty: String?
 
-public struct DiscogsCollectionReleaseLabel: Codable {
-    public var catno: String?
-    public var catalogNumber: String? { return catno }
-    public var entityType: String?
-    public var id: Int
-    public var name: String
-    public var resourceUrl: String
-    
-}
+    }
 
-public struct DiscogsCollectionReleaseNote: Codable {
-    public var fieldId: String
-    public var value: String
-    
+    public struct Information: Codable {
+        public var id: Int
+        public var title: String
+        public var year: Int?
+        public var resourceUrl: String
+        public var thumb: String?
+        public var coverImage: String?
+        public var formats: [Format]?
+        public var labels: [Label]?
+        public var artists: [Artist]?
+
+    }
+
+    public struct Label: Codable {
+        public var catno: String?
+        public var catalogNumber: String? { return catno }
+        public var entityType: String?
+        public var id: Int
+        public var name: String
+        public var resourceUrl: String
+
+    }
+
+    public struct Note: Codable {
+        public var fieldId: String
+        public var value: String
+
+    }
+
 }
