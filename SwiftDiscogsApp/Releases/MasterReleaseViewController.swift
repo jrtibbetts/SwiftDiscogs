930639a686668f14412c60e58fdcf2d2d73f47ba
Jason R Tibbetts <jason@tibbetts.net>
2019-03-08 13:49:09 -0500

    No ticket: Got rid of some duplicate code in MasterReleaseController, and moved much of its implementation into an enclosed SectionedModel extension.

--- SwiftDiscogsApp/Releases/MasterReleaseViewController.swift
+++ SwiftDiscogsApp/Releases/MasterReleaseViewController.swift
@@ -11,10 +11,30 @@ public class MasterReleaseViewController: UITableViewController, DiscogsProvider
     public var discogs: Discogs?
 
     public var masterRelease: MasterRelease? {
+        get {
+            return model.masterRelease
+        }
+        
+        set {
+            model.masterRelease = masterRelease
+        }
+    }
+
+    public var releaseSummary: ReleaseSummary? {
         didSet {
-            if let masterReleaseID = masterRelease?.id {
+            if let masterReleaseID = releaseSummary?.id {
+                // Get the master release itself.
+                discogs?.masterRelease(identifier: masterReleaseID).done { [weak self] (masterRelease) in
+                    self?.model.masterRelease = masterRelease
+                    self?.navigationItem.title = masterRelease.title
+                    self?.tableView.reloadData()
+                    }.catch { (error) in
+                        print("Error: \(error)")
+                    }
+                // Get the master release's versions. We don't have to wait for
+                // the actual master release itself to be retrieved.
                 discogs?.releasesForMasterRelease(masterReleaseID, pageNumber: 1, resultsPerPage: 200).done { [weak self] (releaseSummaries) in
-                    self?.releaseVersions = releaseSummaries.versions
+                    self?.model.releaseVersions = releaseSummaries.versions
                     self?.tableView.reloadData()
                     }.catch { (error) in
                         print("Error: \(error)")
@@ -22,90 +42,81 @@ public class MasterReleaseViewController: UITableViewController, DiscogsProvider
             }
         }
     }
+    
+    // MARK: - Private Properties
+    
+    private var model: Model = Model()
+    
+    // MARK: - UIViewController
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = model
+    }
 
-    public var releaseSummary: ReleaseSummary? {
-        didSet {
-            if let masterReleaseID = releaseSummary?.id {
-                _ = discogs?.masterRelease(identifier: masterReleaseID).done { [weak self] (masterRelease) in
-                    self?.masterRelease = masterRelease
-                    self?.tableView.reloadData()
-                    self?.navigationItem.title = masterRelease.title
-                    }.catch { (error) in
-                        print("Error: \(error)")
-                    }
+    private class Model: SectionedModel {
+
+        // MARK: - Public Properties
+        
+        public var masterRelease: MasterRelease?
+        
+        public var releaseVersions: [MasterReleaseVersion]?
+
+        // MARK: - Private Properties
+
+        private let tracklistSection = Section(cellID: "trackCell", headerText: "Tracklist")
+
+        private let versionsSection = Section(cellID: "versionCell", headerText: "All Releases")
+        
+        // MARK: - Initialization
+        
+        public init() {
+            super.init(sections: [tracklistSection, versionsSection])
+        }
+        
+        // MARK: - UITableViewDataSource
+
+        open override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+            return sections[section].headerText
+        }
+
+        open override func tableView(_ tableView: UITableView,
+                                     cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let section = sections[indexPath.section]
+            let cell = tableView.dequeueReusableCell(withIdentifier: section.cellID,
+                                                     for: indexPath)
+
+            if section === tracklistSection {
+                if let cell = cell as? TracklistTableCell,
+                    let track = masterRelease?.tracklist[indexPath.row] {
+                    cell.setUp(with: track)
+                }
+            } else if section === versionsSection {
+                if let cell = cell as? ReleaseVersionTableCell,
+                    let releaseSummary = releaseVersions?[indexPath.row] {
+                    cell.setUp(with: releaseSummary)
+                }
+            }
+
+            return cell
+        }
+
+        public override func numberOfSections(in tableView: UITableView) -> Int {
+            return sections.count
+        }
+
+        public override func tableView(_ tableView: UITableView,
+                                       numberOfRowsInSection sectionIndex: Int) -> Int {
+            let section = sections[sectionIndex]
+
+            if section === tracklistSection {
+                return masterRelease?.tracklist.count ?? 0
+            } else if section === versionsSection {
+                return releaseVersions?.count ?? 0
+            } else {
+                return 0
             }
         }
-    }
-
-    open class Section: NSObject {
-
-        open var cellID: String
-
-        open var headerText: String? = nil
-
-        public init(cellID: String,
-                    headerText: String? = nil) {
-            self.cellID = cellID
-            self.headerText = headerText
-        }
-
-    }
-
-    public var releaseVersions: [MasterReleaseVersion]?
-
-    // MARK: - Private Properties
-
-    private let tracklistSection = Section(cellID: "trackCell", headerText: "Tracklist")
-
-    private let versionsSection = Section(cellID: "versionCell", headerText: "All Releases")
-
-    private var sections: [Section] {
-        return [tracklistSection, versionsSection]
     }
     
-    // MARK: - UITableViewDataSource
-
-    open override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sections[section].headerText
-    }
-
-    open override func tableView(_ tableView: UITableView,
-                                 cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let section = sections[indexPath.section]
-        let cell = tableView.dequeueReusableCell(withIdentifier: section.cellID,
-                                                 for: indexPath)
-
-        if section === tracklistSection {
-            if let cell = cell as? TracklistTableCell,
-                let track = masterRelease?.tracklist[indexPath.row] {
-                cell.setUp(with: track)
-            }
-        } else if section === versionsSection {
-            if let cell = cell as? ReleaseVersionTableCell,
-                let releaseSummary = releaseVersions?[indexPath.row] {
-                cell.setUp(with: releaseSummary)
-            }
-        }
-
-        return cell
-    }
-
-    // MARK: - UITableViewDataSource
-
-    public override func numberOfSections(in tableView: UITableView) -> Int {
-        return sections.count
-    }
-
-    public override func tableView(_ tableView: UITableView,
-                                   numberOfRowsInSection sectionIndex: Int) -> Int {
-        let section = sections[sectionIndex]
-
-        if section === tracklistSection {
-            return masterRelease?.tracklist.count ?? 0
-        } else if section === versionsSection {
-            return releaseVersions?.count ?? 0
-        } else {
-            return 0
-        }
-    }
 }
