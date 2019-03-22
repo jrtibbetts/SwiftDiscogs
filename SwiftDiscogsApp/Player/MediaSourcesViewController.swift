2245f4aa3d0a426076dd26b9160b90a1b72e7f8f
Jason R Tibbetts <jason@tibbetts.net>
2019-03-21 23:25:23 -0400

    033: Started to implement the MediaSources model and display.

--- SwiftDiscogsApp/Player/MediaSourcesViewController.swift
+++ SwiftDiscogsApp/Player/MediaSourcesViewController.swift
@@ -13,19 +13,112 @@ public class MediaSourcesViewController: CollectionAndTableViewController {
         return model as? MediaSourcesModel
     }
 
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        model = MediaSourcesModel()
+        mediaSourcesDisplay?.model = model
+    }
+
 }
 
 public class MediaSourcesDisplay: CollectionAndTableDisplay {
 
 }
 
+public class MediaSourceTableCell: UITableViewCell {
+
+    @IBOutlet public weak var view: MediaSourceView?
+
+}
+
+public class MediaSourceCollectionCell: UICollectionViewCell {
+
+    @IBOutlet public weak var view: MediaSourceView?
+
+}
+
+public class MediaSourceView: UIView {
+
+    // MARK: - Public Properties
+
+    public var source: MediaSourcesModel.MediaSource? {
+        didSet {
+
+            if let url = source?.iconURL {
+                iconView?.kf.setImage(with: url)
+            } else {
+                nameLabel?.text = source?.name
+            }
+        }
+    }
+
+    // MARK: - Outlets
+
+    @IBOutlet public weak var iconView: UIImageView?
+
+    @IBOutlet public weak var nameLabel: UILabel?
+
+}
+
 public class MediaSourcesModel: CollectionAndTableModel {
 
-    public var sources: [MediaSource]
+    // MARK: - Public Properties
+
+    public var sources: [MediaSource] = [
+        MediaSource(name: "Spotify"),
+        MediaSource(name: "Apple Music"),
+        MediaSource(name: "Amazon Music"),
+        MediaSource(name: "iTunes Library")
+    ]
+
+    // MARK: - Initialization
+
+    public override init() {
+        super.init()
+    }
 
     public init(sources: [MediaSource] = []) {
         self.sources = sources
     }
+
+    // MARK: - UITableViewDataSource
+
+    public override func tableView(_ tableView: UITableView,
+                                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mediaSourceCell",
+                                                 for: indexPath)
+
+        if let cell = cell as? MediaSourceTableCell {
+            cell.view?.source = sources[indexPath.row]
+        }
+
+        return cell
+    }
+
+    // MARK: - UICollectionViewDataSource
+
+    public override func collectionView(_ collectionView: UICollectionView,
+                                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "mediaSourceCell",
+                                                      for: indexPath)
+
+        if let cell = cell as? MediaSourceCollectionCell {
+            cell.view?.source = sources[indexPath.row]
+        }
+
+        return cell
+    }
+
+    // MARK: - CollectionAndTableModel
+
+    public override func numberOfItems(inSection section: Int) -> Int {
+        return sources.count
+    }
+
+    public override func numberOfSections() -> Int {
+        return 1
+    }
+
 
     public class MediaSource: NSObject {
 
