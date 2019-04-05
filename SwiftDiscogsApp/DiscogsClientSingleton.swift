188058801d32c81ece970bad28df5de47fca2978
Jason R Tibbetts <jason@tibbetts.net>
2019-04-04 20:33:50 -0400

    040: Moved the Discogs API keys to environment variables in the scheme, but I'm not checking those in. Instead, I added the environment variables to the Travis-CI project's environment.

--- SwiftDiscogsApp/DiscogsClientSingleton.swift
+++ SwiftDiscogsApp/DiscogsClientSingleton.swift
@@ -5,37 +5,16 @@ import Foundation
 
 public extension DiscogsClient {
 
-    /// The `Codable` struct that's parsed out of the Discogs.plist file.
-    fileprivate struct Properties: Codable {
-        /// The Discogs-issued consumer key hash.
-        var consumerKey: String
-        /// The Discogs-issued secret key hash for this app.
-        var consumerSecret: String
-        /// The Discogs API requires that all requests include a `User-Agent`
-        /// header value.
-        var userAgent: String
-    }
-
     /// A global instance of the `DiscogsClient`. It's initialized with
     /// consumer key, consumer secret, and userAgent properties in a file
     /// named `Discogs.plist`.
     static let singleton: DiscogsClient? = {
-        let bundle = Bundle(for: AppDelegate.self)
-        let plistPath = bundle.url(forResource: "Discogs", withExtension: "plist")!
+        let environment = ProcessInfo.processInfo.environment
+        let client = DiscogsClient(consumerKey: environment["discogsConsumerKey"]!,
+                                   consumerSecret: environment["discogsConsumerSecret"]!,
+                                   userAgent: environment["userAgent"]!)
 
-        do {
-            let propertyData = try Data(contentsOf: plistPath)
-            let properties = try PropertyListDecoder().decode(Properties.self, from: propertyData)
-            let client = DiscogsClient(consumerKey: properties.consumerKey,
-                                       consumerSecret: properties.consumerSecret,
-                                       userAgent: properties.userAgent)
-
-            return client
-        } catch {
-            print("Failed to load the properties from Discogs.plist: \(error.localizedDescription)")
-            
-            return nil
-        }
+        return client
     }()
 
 }
