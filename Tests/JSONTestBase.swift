605e0a3102af88c203de4d2aec450f4c1fb2159f
Jason R Tibbetts <jason@tibbetts.net>
2019-09-30 08:09:43 -0400

    No ticket: The JSONTestBase now uses a snake_case decoder.

--- Tests/JSONTestBase.swift
+++ Tests/JSONTestBase.swift
@@ -8,13 +8,17 @@ public class JSONTestBase: XCTestCase {
 
     func jsonObject<T: Codable>(inLocalJsonFileNamed fileName: String,
                                 inBundle bundle: Bundle = Bundle.main) throws -> T {
-        let jsonUrl = try JSONUtils.url(forFileNamed: fileName, ofType: "json", inBundle: bundle)
+        let jsonUrl = try JSONUtils.url(forFileNamed: fileName, ofType: "json",
+                                        inBundle: bundle)
 
         return try jsonObject(fromUrl: jsonUrl)
     }
 
     func jsonObject<T: Codable>(fromUrl jsonUrl: URL) throws -> T {
-        return try JSONUtils.jsonObject(atUrl: jsonUrl)
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+
+        return try JSONUtils.jsonObject(atUrl: jsonUrl, decoder: jsonDecoder)
     }
 
 }
