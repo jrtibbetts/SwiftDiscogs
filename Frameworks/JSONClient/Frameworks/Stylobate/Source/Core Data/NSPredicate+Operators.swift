c9afe7f232d2986d4c42a43f3187f93de6d56686
Jason R Tibbetts <jason@tibbetts.net>
2021-02-22 01:09:34 -0500

    No ticket: [config] Updated the Travis config to use Xcode 12.2, iPhone 12 Pro, and iOS 14.2.

--- Frameworks/JSONClient/Frameworks/Stylobate/Source/Core Data/NSPredicate+Operators.swift
+++
@@ -1,7 +0,0 @@
-//  Copyright © 2019 Poikile Creations. All rights reserved.
-
-import Foundation
-
-public func + (predicateA: NSPredicate, predicateB: NSPredicate) -> NSCompoundPredicate {
-    return NSCompoundPredicate(andPredicateWithSubpredicates: [predicateA, predicateB])
-}
--- Frameworks/JSONClient/Frameworks/Stylobate/Source/Util/LifecycleError.swift
+++
@@ -1,9 +0,0 @@
-//  Copyright © 2019 Poikile Creations. All rights reserved.
-
-import Foundation
-
-public enum LifecycleError: String, Error {
-
-    case weakSelfOutOfScope = "A weak self went out of scope before the block could be invoked."
-
-}
